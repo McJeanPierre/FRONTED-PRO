12b9c39624fcf0ad69f67ff0b61423f1fd412409
5kene <kenfael@gmail.com>
2024-12-10 19:40:21 -0500

    iguldad

--- src/services/tableApi.ts
+++ src/services/tableApi.ts
@@ -7,5 +7,6 @@ export const tableApi = {
   create: (data: TableFormData) => api.post(`/admin/mesas`, data),
   update: (mesaId: number, data: TableFormData) => api.put(`/admin/mesas/${mesaId}`, data),
   delete: (mesaId: number) => api.delete(`/admin/mesas/${mesaId}`),
-  updateAvailability: (mesaId: number, disponible: boolean) => api.post(`/admin/mesas/${mesaId}/disponibilidad`, { disponible })
+  updateAvailability: (mesaId: number, disponible: boolean) => api.post(`/admin/mesas/${mesaId}/disponibilidad`, { disponible }),
+  getAssociatedRestaurant: () => api.get('/admin/restaurante/asociado'), // Nueva función para obtener el restaurante asociado
 };
